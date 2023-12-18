e082f60d41ee672a4d050d13cf024c7cd013eca1
Sanghyeok Hyun <gmelon24kr@gmail.com>
2023-12-18 21:43:04 +0900

    Title: 정수 사각형 최대 합, Time: 342ms, Memory: 23MB, Status: Passed - CodeTree

--- 231218/정수 사각형 최대 합/maximum-sum-path-in-square.java
+++ 231218/정수 사각형 최대 합/maximum-sum-path-in-square.java
@@ -1,5 +1,36 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] board = new int[n][n];
+        for(int i = 0 ; i < n ; i++) {
+            for(int j = 0 ; j < n ; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+        sc.close();
+
+        // 마지막 위치가 [i][j]일때의 누적합의 최대값
+        int[][] dp = new int[n][n];
+
+        // 초기값 채우기
+        dp[0][0] = board[0][0];
+        for(int i = 1 ; i < n ; i++) {
+            dp[0][i] = dp[0][i - 1] + board[0][i];
+        }
+        for(int i = 1 ; i < n ; i++) {
+            dp[i][0] = dp[i - 1][0] + board[i][0];
+        }
+
+        for(int i = 1 ; i < n ; i++) {
+            for(int j = 1 ; j < n ; j++) {
+                // 위 또는 왼쪽에서의 값을 가져와 비교
+                dp[i][j] = board[i][j] + Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        System.out.println(dp[n - 1][n - 1]);
     }
 }
